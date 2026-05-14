fe559cf2b9b3a0b1a113b88fd4857fdef411236c
foundattionfinancial <mmasri0530@gmail.com>
2026-05-14 14:54:41 -0400

    Update discord.js

--- pages/api/auth/discord.js
+++ pages/api/auth/discord.js
@@ -1,9 +1,12 @@
 export default function handler(req, res) {
+  const { popup } = req.query;
+  
   const params = new URLSearchParams({
     client_id: process.env.DISCORD_CLIENT_ID,
     redirect_uri: process.env.DISCORD_REDIRECT_URI,
     response_type: 'code',
     scope: 'identify guilds',
+    state: popup === 'true' ? 'popup' : 'normal',
   });
   res.redirect(`https://discord.com/api/oauth2/authorize?${params}`);
 }
